751d7355a6b13279508af6423ba01a5d2f2039d9
Dennis Kool <43271994+dkooll@users.noreply.github.com>
2025-03-24 16:13:38 +0100

    feat: format documentation to include type definitions (#31)

--- examples/default/README.md
+++ examples/default/README.md
@@ -1,15 +1,1 @@
-# Default
-
 This example illustrates the default setup, in its simplest form.
-
-## Types
-
-```hcl
-instance = object({
-  resource_group = string
-  location       = string
-  name           = string
-  sku_name       = string
-  sku_tier       = string
-})
-```
--- examples/multiple-ips/main.tf
+++ examples/multiple-ips/main.tf
@@ -20,7 +20,7 @@ module "rg" {
 
 module "network" {
   source  = "cloudnationhq/vnet/azure"
-  version = "~> 4.0"
+  version = "~> 8.0"
 
   naming = local.naming
 
@@ -28,12 +28,12 @@ module "network" {
     name           = module.naming.virtual_network.name
     location       = module.rg.groups.demo.location
     resource_group = module.rg.groups.demo.name
-    cidr           = ["10.18.0.0/16"]
+    address_space  = ["10.18.0.0/16"]
 
     subnets = {
       fw1 = {
-        name = "AzureFirewallSubnet"
-        cidr = ["10.18.0.0/26"]
+        name             = "AzureFirewallSubnet"
+        address_prefixes = ["10.18.0.0/26"]
       }
     }
   }
